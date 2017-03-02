42a7ccd0957dcd2decf03000cf2feffe9bd04980
akocic <you@example.com>
2017-03-02 14:59:40 -0500

    Removed WebLogic properties.

--- cohjmx-client/src/JmxClient.java
+++ cohjmx-client/src/JmxClient.java
@@ -1,10 +1,5 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 
 import javax.management.MBeanServerConnection;
@@ -15,20 +10,14 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
 public class JmxClient {
-  private static final String USER = "user";
-  private static final String PASSWORD = "password";
-  private static final String PROTOCOL_PROVIDER_PACKAGES = "weblogic.management.remote";
 
   public JmxClient() {
     super();
   }
 
   public static void main(String[] args) throws Exception {
-    //    String jmxServiceURL = "service:jmx:iiop://localhost.localdomain:7003/jndi/weblogic.management.mbeanservers.runtime";
-    //    String appName = "clientonboarding-cache-1.0-SNAPSHOT";
-
-    if (args != null && args.length < 3) {
-      System.out.println("Usage: java JmxClient serviceURL appName properties");
+    if (args != null && args.length < 2) {
+      System.out.println("Usage: java JmxClient serviceURL appName");
       System.exit(1);
     }
 
@@ -37,44 +26,23 @@ public class JmxClient {
 
 
     JmxClient jmxClient = new JmxClient();
-    Properties prop = jmxClient.getPropValues(args[2]);
 
     JMXServiceURL url = new JMXServiceURL(jmxServiceURL);
-    String[] creds = { prop.getProperty(USER), prop.getProperty(PASSWORD) };
-
-    Map<String, Object> env = new HashMap<String, Object>();
-    env.put(JMXConnector.CREDENTIALS, creds);
-    env.put(JMXConnectorFactory.PROTOCOL_PROVIDER_PACKAGES, PROTOCOL_PROVIDER_PACKAGES);
-    JMXConnector jmxc = JMXConnectorFactory.connect(url, env);
+    JMXConnector jmxc = JMXConnectorFactory.connect(url);
 
     MBeanServerConnection mBeanServerConnection = jmxc.getMBeanServerConnection();
     System.out.println("Invoking resetStatistics() method on:");
-    jmxClient.resetStatistics(mBeanServerConnection, new StringBuffer("Coherence:type=*,name=").append("\"")
-                                                                                               .append(appName)
-                                                                                               .append(":*\",*")
-                                                                                               .toString());
+    jmxClient.resetStatistics(mBeanServerConnection, new StringBuffer("Coherence:type=*,").append("name=")
+                                                                                          .append("\"")
+                                                                                          .append(appName)
+                                                                                          .append(":*\",*")
+                                                                                          .toString());
 
-    jmxClient.resetStatistics(mBeanServerConnection, new StringBuffer("Coherence:type=*,service=").append("\"")
-                                                                                                  .append(appName)
-                                                                                                  .append(":*\",*")
-                                                                                                  .toString());
-  }
-
-  private Properties getPropValues(String fileName) throws Exception {
-    InputStream inputStream = null;
-    Properties prop = new Properties();
-    try {
-      String propFileName = fileName;
-      inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-      if (inputStream != null) {
-        prop.load(inputStream);
-      } else {
-        throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-      }
-    } finally {
-      inputStream.close();
-    }
-    return prop;
+    jmxClient.resetStatistics(mBeanServerConnection, new StringBuffer("Coherence:type=*,").append("service=")
+                                                                                          .append("\"")
+                                                                                          .append(appName)
+                                                                                          .append(":*\",*")
+                                                                                          .toString());
   }
 
   private void resetStatistics(MBeanServerConnection mBeanServerConnection, String name) throws IOException,
